83f5fa12ee770060384743f8f68eb3525c417c05
Cristian Arganaraz <127353218+Cristian-Xtorm@users.noreply.github.com>
2023-03-08 19:38:21 -0300

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,14 @@
+
+document.getElementById("btn").addEventListener("click",e=>{
+    window.location.href = "#inicio"
+})
+document.getElementById("btn").addEventListener("click",e=>{
+    window.location.href = "#acerca"
+})
+document.getElementById("btn").addEventListener("click",e=>{
+    window.location.href = "#portfolio"
+})
+document.getElementById("btn").addEventListener("click",e=>{
+    window.location.href = "#contacto"
+})
+
